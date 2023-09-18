c55e5f8fe83b7c14812cb3ff0464e597a7c491bb
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2023-09-18 17:29:29 +0300

    Add my existing solution - a budget program, with classes and inheritance (from problem P.6.3).

--- budget.py
+++ budget.py
@@ -1,63 +1,71 @@
 # -----------------------------------------------
-# P.5.5
+# P.6.3
 # -----------------------------------------------
-# • Padaryti biudžeto programą, kuri:
-#   • Leistų vartotojui įvesti pajamas.
-#   • Leistų vartotojui įvesti išlaidas.
-#   • Leistų vartotojui parodyti pajamų/išlaidų balansą.
-#   • Leistų vartotojui parodyti biudžeto ataskaitą (visus pajamų ir išlaidų
-#     įrašus su sumomis).
-#   • Leistų vartotojui išeiti iš programos.
+# • Patobulinti 5 pamokos biudžeto programą taip, kad joje:
 #
-# Rekomendacija, kaip galima būtų padaryti:
-#   • Programa turi turėti klasę Irasas, kuri turėtų argumentus tipas (Pajamos
-#     arba Išlaidos) ir suma. Galima prirašyti str() metodą, kuris grąžintų,
-#     kaip bus atvaizduojamas spausdinamas objektas.
-#   • Programa turi turėti klasę Biudzetas, kurioje būtų:
-#     1. Metodas init(), kuriame sukurtas tuščias sąrašas zurnalas, į kurį bus
-#        dedami sukurti pajamų ir išlaidų objektai.
-#     2. Metodas prideti_pajamu_irasa(self, suma), kuris priimtų paduotą sumą,
-#        sukurtų pajamų objektą ir įdėtų jį į biudžeto žurnalą.
-#     3. Metodas prideti_islaidu_irasa(self, suma), kuris priimtų paduotą sumą,
-#        sukurtų išlaidų objektą ir įdėtų jį į biudžeto žurnalą.
-#     4. Metodas gauti_balansą(self), kuris grąžintų žurnale laikomų pajamų ir
-#        išlaidų balansą.
-#     5. Metodas parodyti_ataskaita(self), kuris atspausdintų visus pajamų ir
-#        išlaidų įrašus (nurodydamas kiekvieno įrašo tipą ir sumą).
+#   • Irasas klasėje būtų tik vienas atributas suma, ir būtų sukurtos dvi
+#     vaikinės klasės PajamuIrasas ir IslaiduIrasas.
+#   • Klasė PajamuIrasas turėtų du papildomus atributus siuntejas ir
+#     papildoma_informacija.
+#   • Klasė IslaiduIrasas turėtų du papildomus atributus atsiskaitymo_budas ir
+#     isigyta_preke_paslauga.
+#   • Klasės Biudzetas metodai parodyti_balansa() ir parodyti_ataskaita() būtų
+#     perdaryti taip, kad jie nuskaitytų kiekvieną įrašą iš žurnalo ir
+#     atpažinę, ar tai pajamų įrašas, ar išlaidų (pvz., naudojant Python
+#     funkciją isinstance()), atspausdintų reikiamą informaciją kaip ir prieš
+#     tai.
+#   • Būtų vartotojo sąsaja per komandinę eilutę (Command-Line Interface (CLI),
+#     kas savo ruožtu yra vienas iš galimų Text-Based User Interface (TUI)
+#     tipų), per kurią vartotojas galėtų įvesti pajamų ir išlaidų įrašus po
+#     vieną ir pamatyti balansą bei ataskaitą.
 # -----------------------
-# • Create a "Budget" program which would:
-#   • Let a user input their income.
-#   • Let a user input their expenses.
-#   • Show the user their Income Statement (the statement that shows the user's
-#     income and expenditures, and also shows whether a user is making
-#     profit or loss for a given period).
-#   • Show the user their Income and Expenditure Account (the detailed summary
-#     of every income and expense, with their type and amount).
-#   • Let the user exit the program.
+# • Modify the "Budget" program from the previous lesson (Python lesson 5,
+#   exercise no. 5) so that it would have:
 #
-# Recommendation on how it could be done:
-#   • The program could have a class Entry with attributes money_type (income
-#     or expenses) and amount. It could also have a str() method which would
-#     return how the object should be printed.
-#   • The program could have a class Budget with the attribute log and the
-#     following methods:
-#     1. init() - it would initialize a log attribute (empty at the beginning)
-#        in which the created income and expenses objects could be placed.
-#     2. add_income_entry(self, amount) - it would get the amount as an input,
-#        create an income object and put it in a budget log.
-#     3. add_expenses_entry(self, amount) - it would get the amount as an
-#        input, create an expenses object and put it in a budget log.
-#     4. print_income_statement(self) - to print the Income Statement.
-#     5. print_ie_account(self) - to print the Income and Expenditure Account.
+#   • The Entry class only have a single attribute amount, and create its child
+#     classes IncomeEntry and ExpensesEntry.
+#   • The IncomeEntry class have two additional attributes sender and
+#     additional_info.
+#   • The ExpensesEntry class have two additional attributes payment_option and
+#     received_good_or_service.
+#   • Refactored Budget class methods print_income_statement() and
+#     print_ie_account() so that they would read every entry in a log and
+#     recognize whether it is an income entry or an expenses entry (for
+#     example, using the isinstance() method) and print the required
+#     information as previously.
+#   • A Command-Line Interface (CLI) (one possible type of a Text-Based User
+#     Interface (TUI)) so that a user, using the command-line, could enter the
+#     income and expenses entries one by one, and view both the Income
+#     Statement and Income and Expenditure Account.
 # -----------------------------------------------
+
 
 class Entry:
-    def __init__(self, money_type, amount):
-        self.money_type = money_type
+    def __init__(self, amount):
         self.amount = amount
 
+
+class IncomeEntry(Entry):
+    def __init__(self, amount, sender, info):
+        super().__init__(amount)
+        self.sender = sender
+        self.info = info
+
     def __str__(self):
-        return f"[{self.money_type}]: {self.amount} €"
+        return (
+            f"Income: {self.amount}, Sender: {self.sender}, Additional info: {self.info}")
+
+
+class ExpensesEntry(Entry):
+    def __init__(self, amount, payment_option, received_good_or_service):
+        super().__init__(amount)
+        self.payment_option = payment_option
+        self.received_good_or_service = received_good_or_service
+
+    def __str__(self):
+        return (
+            f"Expenses: {self.amount}, Payment option: {self.payment_option}, "
+            f"Received good or service: {self.received_good_or_service}")
 
 
 class Budget:
@@ -66,14 +74,14 @@ class Budget:
         self.total_income = []
         self.total_expenses = []
 
-    def add_income_entry(self, amount):
-        entry = Entry("Income", amount)
-        self.total_income.append(amount)
+    def add_income_entry(self, income, sender, info):
+        entry = IncomeEntry(income, sender, info)
+        self.total_income.append(income)
         self.log.append(entry)
 
-    def add_expenses_entry(self, amount):
-        entry = Entry("Expenses", amount)
-        self.total_expenses.append(amount)
+    def add_expenses_entry(self, expenses, payment_option, received_good_or_service):
+        entry = ExpensesEntry(expenses, payment_option, received_good_or_service)
+        self.total_expenses.append(expenses)
         self.log.append(entry)
 
     # a.k.a. balance
@@ -103,31 +111,41 @@ class Budget:
 
 budget = Budget()
 
-while True:
-    try:
-        choice = int(input("Enter the desired action:\n"
-                           "1 - Add income.\n"
-                           "2 - Add expenses.\n"
-                           "3 - View income statement (balance).\n"
-                           "4 - View income and expenditure account (report).\n"
-                           "5 - Quit.\n"
-                           "> "))
 
-        match choice:
-            case 1:
-                income_amount = abs(round(float(input("Enter your income: ")), 2))
-                budget.add_income_entry(income_amount)
-            case 2:
-                expenses_amount = abs(round(float(input("Enter your expenses: ")), 2))
-                budget.add_expenses_entry(expenses_amount)
-            case 3:
-                budget.print_income_statement()
-            case 4:
-                budget.print_ie_account()
-            case 5:
-                print("Goodbye!")
-                break
-            case _:
-                print("Error: Your action should be a number between 1 and 5!")
-    except ValueError:
-        print("Error: Invalid action or value!")
+def main():
+    while True:
+        try:
+            choice = int(input("Enter the desired action:\n"
+                               "1 - Add income.\n"
+                               "2 - Add expenses.\n"
+                               "3 - View income statement (balance).\n"
+                               "4 - View income and expenditure account (report).\n"
+                               "5 - Quit.\n"
+                               "> "))
+
+            match choice:
+                case 1:
+                    income = abs(round(float(input("Enter your income: ")), 2))
+                    sender = str(input("Enter the sender: "))
+                    info = str(input("Enter more info about the transaction: "))
+                    budget.add_income_entry(income, sender, info)
+                case 2:
+                    expenses = abs(round(float(input("Enter your expenses: ")), 2))
+                    payment_option = str(input("Enter the payment option: "))
+                    received_good_or_service = str(input("Enter more info about the receiced good or service: "))
+                    budget.add_expenses_entry(expenses, payment_option, received_good_or_service)
+                case 3:
+                    budget.print_income_statement()
+                case 4:
+                    budget.print_ie_account()
+                case 5:
+                    print("Goodbye!")
+                    break
+                case _:
+                    print("Error: Your action should be a number between 1 and 5!")
+        except ValueError:
+            print("Error: Invalid action or value!")
+
+
+if __name__ == "__main__":
+    main()
