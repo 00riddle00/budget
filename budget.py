23cc9a5a74c54f118efe1ae6f703199ec8581a9e
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2023-10-10 16:11:41 +0300

    Add database object and models to Flask shell context.

--- budget.py
+++ budget.py
@@ -1,11 +1,27 @@
+#!/usr/bin/env python3
+
+"""Here the Flask application instance is defined."""
+
 import os
 
 from app import create_app, db
+from app.models import Expenses, Income, User
 from flask_migrate import Migrate
 
 config_name = os.getenv("FLASK_CONFIG") or "default"
 app = create_app(config_name)
 migrate = Migrate(app, db)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    """Add database object and models to Flask shell context."""
+    return dict(
+        db=db,
+        User=User,
+        Income=Income,
+        Expenses=Expenses,
+    )
 
 
 @app.cli.command()
@@ -16,10 +32,6 @@ def test():
     tests = unittest.TestLoader().discover("tests")
     unittest.TextTestRunner(verbosity=2).run(tests)
 
-
-# @app.shell_context_processor
-# def make_shell_context():
-# 	return dict(db=db, User=User)
 
 if __name__ == "__main__":
     app.run(debug=True)
